fbe04ede36ca18473384e3b2693a2bda1dc9880b
Jitse De Smet <jitse@reedonk8.be>
2025-12-11 14:02:12 +0100

    add more tests about CRDT behaviour

--- packages/utils-crdt-store/test/WebSyncedStore-test.ts
+++ packages/utils-crdt-store/test/WebSyncedStore-test.ts
@@ -41,6 +41,32 @@ describe('Web Synced Store', () => {
     await expect(getIter(crdt1).toArray()).resolves.toHaveLength(0);
   });
 
+  it('store B can remove what A and B have created', async() => {
+    // Remote is now empty.
+    const DF1 = new DataFactoryUuid();
+    const DF2 = new DataFactoryUuid();
+    const crdt1 = new WebSyncedStore({ dataFactory: DF1, webSource: 'http://localhost:3000/test.nq' });
+    const crdt2 = new WebSyncedStore({ dataFactory: DF2, webSource: 'http://localhost:3000/test.nq' });
+    await crdt1.pullData();
+    await crdt2.pullData();
+
+    // 1&2 makes, 1 sends, 2 fetches and removes, 1 fetches and is removed
+    // Test would fail using merge: PN counter with sum of both counters.
+    const testTriple = DF.quad(DF.namedNode(`${prefix}c`), DF.namedNode(`${prefix}c`), DF.namedNode(`${prefix}c`));
+    await eventToPromise(crdt1.import(wrap([ testTriple ])));
+    await eventToPromise(crdt2.import(wrap([ testTriple ])));
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(1);
+    await expect(getIter(crdt2).toArray()).resolves.toHaveLength(1);
+    await crdt1.pushData();
+    await crdt2.pullData();
+    await eventToPromise(crdt2.removeMatches());
+    await crdt2.pushData();
+    await crdt1.pullData();
+
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(0);
+    await expect(getIter(crdt2).toArray()).resolves.toHaveLength(0);
+  });
+
   it('store A adding and removing constantly, while B adds once, result in no remove', async() => {
     // Remote is now empty.
     const DF1 = new DataFactoryUuid();
@@ -50,6 +76,8 @@ describe('Web Synced Store', () => {
     await crdt1.pullData();
     await crdt2.pullData();
 
+    // 1 adds and removes a bunch of times; b adds once, both sync and see the triple from b.
+    // Would fail when: using PN counter merging using max.
     const testTriple = DF.quad(DF.namedNode(`${prefix}b`), DF.namedNode(`${prefix}b`), DF.namedNode(`${prefix}b`));
     for (let i = 0; i < 10; i++) {
       await eventToPromise(crdt1.import(wrap([ testTriple ])));
@@ -63,6 +91,43 @@ describe('Web Synced Store', () => {
     await crdt2.pullData();
     await crdt2.pushData();
     await crdt1.pullData();
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(1);
+    await expect(getIter(crdt2).toArray()).resolves.toHaveLength(1);
+  });
+
+  it('store A adding and removing, store B removes, and after sync both add', async() => {
+    // Remote is now empty.
+    const DF1 = new DataFactoryUuid();
+    const DF2 = new DataFactoryUuid();
+    const crdt1 = new WebSyncedStore({ dataFactory: DF1, webSource: 'http://localhost:3000/test.nq' });
+    // Clear server
+    await crdt1.pullData();
+    await eventToPromise(crdt1.removeMatches());
+    await crdt1.pushData();
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(0);
+    const crdt2 = new WebSyncedStore({ dataFactory: DF2, webSource: 'http://localhost:3000/test.nq' });
+    await crdt1.pullData();
+    await crdt2.pullData();
+
+    // 1 adds and removes; b removes, both sync and add after.
+    // Would fail when: using PN counter merging using max.
+    const testTriple = DF.quad(DF.namedNode(`${prefix}d`), DF.namedNode(`${prefix}d`), DF.namedNode(`${prefix}d`));
+    await eventToPromise(crdt1.import(wrap([ testTriple ])));
+    await eventToPromise(crdt1.remove(wrap([ testTriple ])));
+    await eventToPromise(crdt2.remove(wrap([ testTriple ])));
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(0);
+    await expect(getIter(crdt2).toArray()).resolves.toHaveLength(0);
+
+    // 2 way sync
+    await crdt1.pushData();
+    await crdt2.pullData();
+    await crdt2.pushData();
+    await crdt1.pullData();
+    await expect(getIter(crdt1).toArray()).resolves.toHaveLength(0);
+    await expect(getIter(crdt2).toArray()).resolves.toHaveLength(0);
+
+    await eventToPromise(crdt1.import(wrap([ testTriple ])));
+    await eventToPromise(crdt2.import(wrap([ testTriple ])));
     await expect(getIter(crdt1).toArray()).resolves.toHaveLength(1);
     await expect(getIter(crdt2).toArray()).resolves.toHaveLength(1);
   });
